e491fffd1467ebdd10157104310b8d8d78199f4a
Kyung Lee <46550288+kyunglee1@users.noreply.github.com>
2021-01-21 21:17:17 -0800

    Add fetch to API

--- src/SearchBar/index.js
+++ src/SearchBar/index.js
@@ -1,1 +1,20 @@
-export default function SearchBar() {}
+/* eslint-disable react/prop-types */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/jsx-filename-extension */
+
+import React from 'react';
+
+export default function SearchBar({ value, onInputChange, onSearchClick }) {
+  const handleChange = (e) => {
+    onInputChange(e);
+  };
+  const handleClick = () => {
+    onSearchClick();
+  };
+  return (
+    <>
+      <input type="text" value={value} onChange={handleChange} />
+      <button onClick={handleClick}>search</button>
+    </>
+  );
+}
--- src/WeatherTable/index.js
+++ src/WeatherTable/index.js
@@ -1,1 +1,20 @@
-export default function WeatherTable() {}
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+export default function WeatherTable() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th colSpan="2">Weather</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Temp:</td>
+          <td>Feels like:</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
