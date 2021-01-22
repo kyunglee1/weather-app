ac1762db71e7661855770a6cf2656cdda4bb5956
Kyung Lee <46550288+kyunglee1@users.noreply.github.com>
2021-01-21 17:02:17 -0800

    Add base component files

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,16 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import SearchBar from '../SearchBar/index';
+import WeatherTable from '../WeatherTable/index';
 import './index.css';
 
-function App() {}
+function App() {
+  return (
+    <div>
+      <SearchBar />
+      <WeatherTable />
+    </div>
+  );
+}
 
 export default App;
