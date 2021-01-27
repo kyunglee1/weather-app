39df0a46192420796ab7d8d1500b1f72906c47b2
Kyung Lee <46550288+kyunglee1@users.noreply.github.com>
2021-01-26 16:44:11 -0800

    Move async function inside useEffect

--- src/WeatherTable/index.js
+++ src/WeatherTable/index.js
@@ -20,40 +20,47 @@ export default function WeatherTable({ location, filters }) {
       visibility: '',
     },
   });
-  useEffect(async () => {
-    try {
+  useEffect(() => {
+    const fetchData = async () => {
       const res = await fetch(
         `//api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=df8f43f7523fec63b2b7896097360962`
       );
+      if (!res.ok) {
+        throw new Error('Bad response from server.');
+      }
       const data = await res.json();
-      const temperature = Math.round(data.main.temp);
-      const { description } = data.weather[0];
-      const { icon } = data.weather[0];
-      const feelsLike = Math.round(data.main.feels_like);
-      const tempMax = Math.round(data.main.temp_max);
-      const tempMin = Math.round(data.main.temp_min);
-      const { humidity } = data.main;
-      const wind = Math.round(data.wind.speed);
-      const { visibility } = data;
-
-      setWeather({
-        temperature,
-        tempMax,
-        tempMin,
-        description,
-        icon,
-        advanced: {
-          feelsLike,
-          humidity,
-          wind,
-          visibility,
-        },
+      return data;
+    };
+    fetchData()
+      .then((data) => {
+        const temperature = Math.round(data.main.temp);
+        const { description } = data.weather[0];
+        const { icon } = data.weather[0];
+        const feelsLike = Math.round(data.main.feels_like);
+        const tempMax = Math.round(data.main.temp_max);
+        const tempMin = Math.round(data.main.temp_min);
+        const { humidity } = data.main;
+        const wind = Math.round(data.wind.speed);
+        const { visibility } = data;
+        setWeather({
+          temperature,
+          tempMax,
+          tempMin,
+          description,
+          icon,
+          advanced: {
+            feelsLike,
+            humidity,
+            wind,
+            visibility,
+          },
+        });
+      })
+      .catch((err) => {
+        alert(err.message);
+        setWeather((prev) => ({ ...prev, temperature: 'N/A' }));
       });
-    } catch (err) {
-      alert(err);
-      setWeather((prev) => ({ ...prev, temperature: 'N/A' }));
-    }
-  }, []);
+  }, [location]);
 
   return (
     <table className="weather-table">
