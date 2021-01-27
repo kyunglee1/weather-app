9c3f1e045d955dc6ee7aae041509c52389089e40
Kyung Lee <46550288+kyunglee1@users.noreply.github.com>
2021-01-26 17:17:11 -0800

    Extract useWeather hook from WeatherTable

--- src/WeatherTable/index.js
+++ src/WeatherTable/index.js
@@ -1,67 +1,13 @@
-/* eslint-disable no-alert */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-filename-extension */
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import useWeather from '../hooks/weather';
 import WeatherFieldRow from '../WeatherFieldRow/index';
 import WeatherIcon from '../WeatherIcon/index';
 import './index.css';
 
 export default function WeatherTable({ location, filters }) {
-  const [weather, setWeather] = useState({
-    temperature: '---',
-    tempMax: '--',
-    tempMin: '--',
-    description: '',
-    icon: '',
-    advanced: {
-      feelsLike: '',
-      humidity: '',
-      wind: '',
-      visibility: '',
-    },
-  });
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch(
-        `//api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=df8f43f7523fec63b2b7896097360962`
-      );
-      if (!res.ok) {
-        throw new Error('Bad response from server.');
-      }
-      const data = await res.json();
-      return data;
-    };
-    fetchData()
-      .then((data) => {
-        const temperature = Math.round(data.main.temp);
-        const { description } = data.weather[0];
-        const { icon } = data.weather[0];
-        const feelsLike = Math.round(data.main.feels_like);
-        const tempMax = Math.round(data.main.temp_max);
-        const tempMin = Math.round(data.main.temp_min);
-        const { humidity } = data.main;
-        const wind = Math.round(data.wind.speed);
-        const { visibility } = data;
-        setWeather({
-          temperature,
-          tempMax,
-          tempMin,
-          description,
-          icon,
-          advanced: {
-            feelsLike,
-            humidity,
-            wind,
-            visibility,
-          },
-        });
-      })
-      .catch((err) => {
-        alert(err.message);
-        setWeather((prev) => ({ ...prev, temperature: 'N/A' }));
-      });
-  }, [location]);
-
+  const weather = useWeather(location);
   return (
     <table className="weather-table">
       <thead>
