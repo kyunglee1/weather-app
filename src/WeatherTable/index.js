1434a1cb060f0611bdb054099edc1256b19ffece
Kyung Lee <46550288+kyunglee1@users.noreply.github.com>
2021-01-28 19:23:21 -0800

    Add responsive font size

--- src/WeatherTable/index.js
+++ src/WeatherTable/index.js
@@ -9,52 +9,54 @@ import './index.css';
 const WeatherTable = ({ location, filters }) => {
   const weather = useWeather(location);
   return (
-    <table className="weather-table">
-      <thead>
-        <tr>
-          <th colSpan="2">{location.toUpperCase()}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td id="temperature-td">{`${weather.temperature}°`}</td>
-          <td>
-            <div id="icon-td">
-              <WeatherIcon type={weather.icon} />
-              {weather.description}
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td>{`HI ${weather.tempMax}°`}</td>
-          <td>{`LO ${weather.tempMin}°`}</td>
-        </tr>
-        {filters.feelsLike && (
-          <WeatherFieldRow
-            fieldName="Feels Like"
-            fieldData={`${weather.advanced.feelsLike}°`}
-          />
-        )}
-        {filters.wind && (
-          <WeatherFieldRow
-            fieldName="Wind Speed"
-            fieldData={`${weather.advanced.wind} mph`}
-          />
-        )}
-        {filters.humidity && (
-          <WeatherFieldRow
-            fieldName="Humidity"
-            fieldData={`${weather.advanced.humidity}%`}
-          />
-        )}
-        {filters.visibility && (
-          <WeatherFieldRow
-            fieldName="Visibility"
-            fieldData={`${weather.advanced.visibility} m`}
-          />
-        )}
-      </tbody>
-    </table>
+    <div className="table-wrapper">
+      <table className="weather-table">
+        <thead>
+          <tr>
+            <th colSpan="2">{location.toUpperCase()}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td id="temperature-td">{`${weather.temperature}°`}</td>
+            <td>
+              <div id="icon-td">
+                <WeatherIcon type={weather.icon} />
+                {weather.description}
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>{`HI ${weather.tempMax}°`}</td>
+            <td>{`LO ${weather.tempMin}°`}</td>
+          </tr>
+          {filters.feelsLike && (
+            <WeatherFieldRow
+              fieldName="Feels Like"
+              fieldData={`${weather.advanced.feelsLike}°`}
+            />
+          )}
+          {filters.wind && (
+            <WeatherFieldRow
+              fieldName="Wind Speed"
+              fieldData={`${weather.advanced.wind} mph`}
+            />
+          )}
+          {filters.humidity && (
+            <WeatherFieldRow
+              fieldName="Humidity"
+              fieldData={`${weather.advanced.humidity}%`}
+            />
+          )}
+          {filters.visibility && (
+            <WeatherFieldRow
+              fieldName="Visibility"
+              fieldData={`${weather.advanced.visibility} m`}
+            />
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
